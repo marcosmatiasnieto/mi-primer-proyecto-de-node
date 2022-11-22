7de37f5cb3c7d445d744c814cf4d3453f18acf1d
Marcos <nieto.marcos.ed@gmail.com>
2022-11-22 10:19:51 -0300

    Modificacion Tarea 2 de OpenBootCamp

--- alumno.js
+++ alumno.js
@@ -1,2 +1,13 @@
-var alumno = "marcos";
+var alumno = [
+    "Marcos Matias", 
+    26, 
+    true,
+    fecha_nac= new Date(1996,07,15),
+    libro={
+        titulo: "El alquimista",
+        autor:"Paulo Coelho",
+        fecha: new Date(1988,11),
+        url:"http://formarse.com.ar/libros/Libros%20para%20descargar%20de%20maestros%20espirituales/El-Alquimista.pdf"
+    }
+]
 console.log(alumno);
